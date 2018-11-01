20e0fb02b1a03a97d6d7d30607ce042b8972d6a2
azasaoqyy <azasaoqyy1231@gmail.com>
2018-11-01 11:21:17 +0800

    啥都沒有
    基本沒啥功能，activity目前只有起始頁面及預想要做會員中心的部分。

---
+++ program/AV_Delivery_v10/app/src/main/java/com/example/bjt/av_delivery_v10/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.bjt.av_delivery_v10;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button account;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        account = findViewById(R.id.account_button);
+        processControllers();
+
+
+    }
+
+    private void processControllers() {
+        View.OnClickListener itemListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(view.getContext(),"WW",Toast.LENGTH_LONG).show();
+                Intent i = new Intent(view.getContext(), AccountActivity.class);
+                startActivity(i);
+            }
+        };
+        // 註冊選單項目點擊監聽物件
+        account.setOnClickListener(itemListener);
+    }
+}
