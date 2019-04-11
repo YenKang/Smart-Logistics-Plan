db3b5f78f70a7369100e6327a11bd3078832853a
YenKang <rcd1988715@gmail.com>
2019-04-11 15:33:35 +0800

    vehicleGetPosition

--- eclipse workspace/java_wsClient/java_wsclient/Main.java
+++ eclipse workspace/java_wsClient/java_wsclient/Main.java
@@ -23,8 +23,8 @@ import de.tudresden.ws.TraasWS;
 public class Main {
 
 	static String sumo_bin = "C:\\Program Files (x86)\\Eclipse\\Sumo\\bin\\sumo-gui.exe";
-	static String config_file = "simulation\\config.sumo.cfg";
-	//static String config_file = "C:\\Users\\yen\\Desktop\\src\\de\\tudresden\\ws\\simulation\\config.sumo.cfg";
+	//static String config_file = "simulation\\config.sumo.cfg";
+	static String config_file = "C:\\Users\\yen\\Desktop\\src\\de\\tudresden\\ws\\simulation\\config.sumo.cfg";
 	static double step_length = 0.01;		
 
 	public static void main(String[] args) {
@@ -40,24 +40,24 @@ public class Main {
 		ws.addOption("step-length", step_length+"");
 		ws.start("user");
 			
-		for(int i=0; i<3600; i++){
+		for(int i=0; i<360000; i++){
 				
 			ws.doTimestep();
 			//System.out.println("test");
-			if (i%10==0) {
-				ws.vehicleAdd("v"+i, "car", "r1", i, 0, 15, (byte) 1);
-				double a  =ws.vehicleGetSpeed("v"+i);
-				
-				System.out.println(a);
-			}
-			/*try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}*/
+			if (i%1000==0) {
+				ws.vehicleAdd("v"+i, "car", "r1", i, 0, 0.1, (byte) 1); //0.01 m/s
 			
-		
+				//System.out.println("the person Number of v0"+ws.vehicleGetPersonNumber("v1000"));
+				System.out.println(ws.vehicleGetLanePosition("v0"));
+				double v0_X=ws.vehicleGetPosition("v0").getX();
+				double v0_Y=ws.vehicleGetPosition("v0").getY();
+				System.out.println("v0 position:"+ "(X:"+v0_X+ ", Y"+ v0_Y+")");
+
+
+				//System.out.println(a);
+			}
+
+	
 		}
 			
 		ws.stop("user");
