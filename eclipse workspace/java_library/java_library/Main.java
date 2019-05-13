993c582c4fc28fc5dba56b3f6e30417e125eeb72
YenKang <rcd1988715@gmail.com>
2019-05-13 15:50:20 +0800

    check boxSize and compute minimum distance with HashMap

--- eclipse workspace/java_library/java_library/Main.java
+++ eclipse workspace/java_library/java_library/Main.java
@@ -18,6 +18,7 @@
 /****************************************************************************/
 
 import java.util.Calendar;
+import java.util.Collections;
 import java.text.SimpleDateFormat;
 
 import it.polito.appeal.traci.SumoTraciConnection;
@@ -27,6 +28,7 @@ import de.tudresden.sumo.cmd.Vehicle;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -72,8 +74,9 @@ public class Main {
 			conn.setOrder(1);
 			/////////////////////
 			
-			double minDistance=0;
+		
 			double min=0;
+			double minDistance=0;
 			
 			String pickVeh="";
 			ArrayList<Double> myList = new ArrayList();
@@ -203,7 +206,7 @@ public class Main {
 					
 					System.out.println("timeStep:"+ timeStep);
 					
-					/*
+					/* boxIndex initialization of setParameter
 					for(int q=1;q<31;q++) {
 						String vehID = Integer.toString(q);
 						// examine the small size boxes
@@ -229,36 +232,31 @@ public class Main {
 					} 
 					
 					
-					/*
-					for(int k=1;k<31;k++) {
-						String vehID = Integer.toString(k);
-						SumoPosition2D veh_Position = (SumoPosition2D)conn.do_job_get(Vehicle.getPosition(vehID));
-						double eachDistance = (double)(conn.do_job_get(Simulation.getDistance2D(8465, 6338, veh_Position.x, veh_Position.y, false, true)));
-						System.out.println(k+ "," + "eachDistance:" + eachDistance );
+					List<Double> alist = new ArrayList<Double>();
 					
-						myList.add(eachDistance);
-						min = myList.get( 0);				
-					
+					for(String vehID : CarsWithAvailableBox.keySet()) {
+						double distance = CarsWithAvailableBox.get(vehID);
+						alist.add(distance);
+						System.out.println("alist:"+ alist);
+						
 					}
-					*/
+					double minValueInList = Collections.min(alist); 
+					System.out.println("minValueInList:"+ minValueInList);
 					
-					/*
-					for(int j=0; j<30;j++) {
-						if(myList.get(j)<min) {
-							min =myList.get(j);
-						}
-					}
-					*/
+				   for(String vehID : CarsWithAvailableBox.keySet()) {
+					  double value = CarsWithAvailableBox.get(vehID);
+					  if(value==minValueInList) {
+						  System.out.println(vehID + " IS THE DISPATCHING CAR!");
+					  }
+							 
+				   }
 					
-					/*
-					System.out.println("min:"+ min);
-					int a = myList.indexOf(min)+1;
-					System.out.println("min car Index:"+ a);
-					pickVeh = Integer.toString(a);
+			
 					
 					// if  a container existed in boxIndex1
-					conn.do_job_set(Vehicle.setParameter(pickVeh,  "boxIndex1", "1"));
+					//conn.do_job_set(Vehicle.setParameter(pickVeh,  "boxIndex1", "1"));
 					
+					/*
 					for(int boxIndex=1;boxIndex<4;boxIndex++ ) {
 						String boxValue= (String)conn.do_job_get(Vehicle.getParameter(pickVeh, "boxIndex"+boxIndex));
 						if(boxValue.equals("0")) {
